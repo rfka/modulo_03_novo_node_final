3ac12da5dc0270cd4464ec4bed098e98c2bcde7a
rfka <kaminski.lista@gmail.com>
2021-09-15 15:58:03 -0300

    atualizacao no trabalho no trabalho

--- models/tdlist.js
+++ models/tdlist.js
@@ -1,11 +1,13 @@
 const mongoose = require ('mongoose');
 
+const Schema = mongoose.Schema;
+
 const tdListModelo = new mongoose.Schema({
-    tarefa: {type: 'String', required: true},
+    nome: {type: 'String', required: true},
     descricao: {type: 'String', required: true},
     dataCriacao: {type: 'Date', default: Date.now},
-    dataConclusao: {type: 'Date'},
-    situacao: {type: 'String', required: true}
+    dataAlteracao: {type: 'Date'},
+    situacaoTarefa: {type: 'String', required: true}
 })
 
 const TdList = mongoose.model('tdList', tdListModelo);
